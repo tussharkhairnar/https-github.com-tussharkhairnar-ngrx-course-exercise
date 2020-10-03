b1893a7c9de623f630cb6009cf125539c3445c9e
Tushar Khairnar <tushar.khairnar@itariumtech.com>
2020-10-03 21:11:57 +0530

    improved state interface

--- src/app/auth/types/auth-state.interface.ts
+++ src/app/auth/types/auth-state.interface.ts
@@ -1,3 +1,8 @@
-export interface AuthStateInterface{
-    isSubmitting: boolean;
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
+import { BackendErrorInterface } from 'src/app/shared/types/backendErrors.interface';
+export interface AuthStateInterface {
+    isSubmitting: boolean
+    currentUser:CurrentUserInterface | null
+    isLoggedIn:boolean | null
+    ValidationErrors: BackendErrorInterface | null
 }
