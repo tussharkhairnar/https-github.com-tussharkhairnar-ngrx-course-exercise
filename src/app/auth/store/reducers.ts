600e3b8c28ad290c8417e3ea0c7f3f1e6be3d2ee
Tushar Khairnar <tushar.khairnar@itariumtech.com>
2020-10-03 21:12:46 +0530

    Updated Reducer : Registared success and failure action

--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -1,22 +1,39 @@
 import { createReducer, on, Action } from '@ngrx/store';
+
 import { AuthStateInterface } from "../types/auth-state.interface";
-import { registerAction } from './actions'
+import { registerAction, registerFailureAction, registerSuccessAction } from './actions'
 
 const initialState: AuthStateInterface = {
-    isSubmitting: false
+    isSubmitting: false,
+    currentUser:null,
+    isLoggedIn:null,
+    ValidationErrors:null
 }
+
 //NOTE: Reducer is a function which will change our state somehow
-
-
 const authReducer = createReducer(
-    initialState, 
-    on(registerAction, (state): AuthStateInterface => ({
-    ...state,
-    isSubmitting: true
+    initialState,
+    on( registerAction, (state): AuthStateInterface => ({
+        ...state,
+        isSubmitting: true
     })
-    )
+    ),
+    on( registerSuccessAction, (state,action):AuthStateInterface=>({
+        ...state,
+        isSubmitting:false,
+        isLoggedIn:true,
+        currentUser: action.currentUser
+    })),
+    on( registerFailureAction, (state,action):AuthStateInterface=>({
+        ...state,
+        isSubmitting:false,
+        isLoggedIn:false,
+        ValidationErrors:action.errors
+    }))
+
 )
 
-export function reducers(state:AuthStateInterface, action:Action){
-    return authReducer(state,action)
+
+export function reducers(state: AuthStateInterface, action: Action) {
+    return authReducer(state, action)
 }
