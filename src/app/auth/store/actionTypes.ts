ca04fac49af2720a78dfb5cdd99848828eb0b963
Tushar Khairnar <tushar.khairnar@itariumtech.com>
2020-10-30 07:41:50 +0530

    Added login actions

--- src/app/auth/store/actionTypes.ts
+++ src/app/auth/store/actionTypes.ts
@@ -2,4 +2,8 @@ export enum ActionTypes  {
     REGISTER = '[Auth] Register',
     REGISTER_FAILURE = '[Auth] Register failure',
     REGISTER_SUCCESS = '[Auth] Register Success',
+
+    Login = '[Auth] Login',
+    Login_FAILURE = '[Auth] Login failure',
+    Login_SUCCESS = '[Auth] Login Success',
 }
