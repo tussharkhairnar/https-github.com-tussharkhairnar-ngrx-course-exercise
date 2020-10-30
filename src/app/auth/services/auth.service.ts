faaef631ba4d7fd30586e9e13e9640441576e28a
Tushar Khairnar <tushar.khairnar@itariumtech.com>
2020-10-30 07:34:52 +0530

    added login request interface and function

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { RegisterRequestInterface } from './../types/register-request.interface';
+import { LoginRequestInterface } from './../types/login-request.interface';
 import { CurrentUserInterface } from './../../shared/types/currentUser.interface'
 import { environment } from '../../../environments/environment.prod';
 import { AuthReponseInterface } from '../types/auth-response.interface';
@@ -14,7 +15,6 @@ export class AuthService {
 
     constructor(private httpClient: HttpClient) { }
 
-    //TODO : Create effect to make API call
     register(data: RegisterRequestInterface): Observable<CurrentUserInterface> {
         const url = `${environment.apiUrl}users`
         return this.httpClient
@@ -22,6 +22,12 @@ export class AuthService {
             .pipe(map((response: AuthReponseInterface) => response.user))
     }
 
+    login(data:LoginRequestInterface):Observable<CurrentUserInterface>{
+        const url = `${environment.apiUrl}user/login`
+        return this.httpClient
+            .post<AuthReponseInterface>(url,data)
+            .pipe(map( (response: AuthReponseInterface) => response.user))
+    }
 
 }
 
